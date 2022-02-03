114d834dd436b725f771b728269e4d086fe03dbd
giorgi <giorgi.doreuli3721@gmail.com>
2022-02-04 02:59:46 +0400

    added Navbar.js file

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Navbar.css'
+
+function Navbar() {
+  return(
+  <div>
+      <div className='navbar'>
+            <h3>Movies</h3>
+            <h3>Favourites</h3>
+      </div>
+  </div>
+  )
+}
+
+export default Navbar;
