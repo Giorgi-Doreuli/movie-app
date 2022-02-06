b25f6d9deb0f71a55a58c9f37581c9e0ddf33153
giorgi <giorgi.doreuli3721@gmail.com>
2022-02-06 18:47:33 +0400

    searched movies list

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,15 +1,52 @@
-import React from 'react';
+import React,  { useState } from 'react';
 import './MovieList.css'
+import FavouriteMovies from './FavouriteMovies'
 
 function MovieList(props) {
+    const [favouriteMovies, setFavouriteMovies] = useState([]);
+    const [showFavourites, setShowFavourites] = useState(false);
+    let counter = 0;
+
+
+    const addToFavourites = (movies) => {
+        for(let i = 0; i < favouriteMovies.length ; i++){
+            if(favouriteMovies[i] === movies){
+                counter++;
+            } 
+        }
+
+        if(counter === 0){
+            setFavouriteMovies(prev => [...prev, movies]);
+        }
+        counter = 0;
+        setShowFavourites(true);
+    }
+
+
+    const removeFavourites = (movies) => {
+        setFavouriteMovies(favouriteMovies.filter(item => item.Poster !== movies.Poster));
+      }
+    
+    
+
+    
   return( 
         <>
-            {props.list.map((movie) => (
-                <div className='List'>
-                    <h2 key='title' >{movie.Title}</h2>
-                    <img key='poster' src={movie.Poster} alt='movie_Poster'/>
+            <div className='moviesList'>
+                <h3>Movies</h3>
+                <div className='movies'>
+                    {props.list.map((moviePosters) => (
+                        <div className='List'>
+                            <img className='moviePoster' key='poster' src={moviePosters.Poster} alt='movie_Poster' onClick={() => addToFavourites(moviePosters)}/>
+                        </div>
+                    ))}
                 </div>
-            ))}
+
+                <h3>Favourites </h3>
+
+                {showFavourites ? <FavouriteMovies favourites = {favouriteMovies} handleClick={removeFavourites} /> : <h4>favourites section is empty, you can add or remove favourite movies by clicking on them</h4>}
+            
+            </div>
         </>
   )
 }
