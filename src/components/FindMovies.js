babbe173d0fda7c6fe60559f806b4817aefba068
giorgi <giorgi.doreuli3721@gmail.com>
2022-02-06 18:46:31 +0400

    added search box

--- src/components/FindMovies.js
+++ src/components/FindMovies.js
@@ -2,6 +2,7 @@ import React, { useState} from 'react';
 import MovieList from './MovieList';
 import './FindMovies.css'
 
+
 function FindMovies() {
   
   const [posts, setPosts] = useState([]);
@@ -21,17 +22,18 @@ function FindMovies() {
         const finalMovie = await response.json();
         setPosts(finalMovie.Search);
     }
- 
+
+   
     
 
   return(
      <div className='fullPage'>
         <div className='searchBar'>
             <input className = 'searchMovies' type="text" placeholder="search movie..." onChange={e => searchMovies(e.target.value)} />
-            <button className='searchBtn btn' onClick={submitValue}>Submit</button>
+            <button className='searchBtn btn' onClick={submitValue}>Search</button>
         </div>
         <div className='moviesList'>
-            {showList ? <MovieList list = {posts} /> : <h4>loading...</h4>}
+            {showList ? <MovieList list = {posts} /> : ""}
         </div>
       </div>
   )
