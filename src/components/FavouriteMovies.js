46268822973e9a0e78e30c7d69e5e9ca4c2c90cd
giorgi <giorgi.doreuli3721@gmail.com>
2022-02-06 18:45:31 +0400

    added favourite movies section

--- src/components/FavouriteMovies.js
+++ src/components/FavouriteMovies.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './FavouriteMovies.css'
 
-function FavouriteMovies() {
-  return <div>
-
-  </div>;
-}
+function FavouriteMovies(props) {
+  
+  return (
+    <div className='favouriteMovies'>
+          {props.favourites.map((moviePosters) => (
+              <div className='favouritesList'> 
+                  <img className='moviePoster' key='poster' src={moviePosters.Poster} alt='movie_Poster' onClick={() => props.handleClick(moviePosters)}/>
+              </div>    
+              ))}
+    </div>
+  )}
 
 export default FavouriteMovies;
